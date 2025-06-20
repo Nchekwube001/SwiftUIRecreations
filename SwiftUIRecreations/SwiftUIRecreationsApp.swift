873020b04a07ad14d02a3d7c95f8922e983809ee
Unekwe Francis <unekwenchekwube@gmail.com>
2025-06-20 13:45:32 +0100

    Initial Commit

---
+++ SwiftUIRecreations/SwiftUIRecreationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRecreationsApp.swift
+//  SwiftUIRecreations
+//
+//  Created by francis on 20/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRecreationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIRecreationsTests/SwiftUIRecreationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRecreationsTests.swift
+//  SwiftUIRecreationsTests
+//
+//  Created by francis on 20/06/2025.
+//
+
+import Testing
+@testable import SwiftUIRecreations
+
+struct SwiftUIRecreationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
